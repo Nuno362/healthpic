5f2b81e51ba36b0318dc7eb22a4375e25a753b25
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2022-11-26 14:05:45 +0000

    rouded cards

--- src/Pages/HomeMenu.js
+++ src/Pages/HomeMenu.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import { Link} from "react-router-dom"
+import  "./homeMenuStyle.css"
 
 function HomeMenu() {
   return (
     <>
-    <header>
+    <header className = "homeMenuHeader">
         <div className ="homeMenuLogo">
           <img className='logo' src='logo192.png' alt='logo'/>
         </div>
     </header>
     <p className = "homeMenuText">
+        Menu <br/>
         Escolha uma das opções abaixo<br/>
         Tempo de seleção: 3 segundos<br/>
     </p>
- 
+    <div class = "card-grid">
+      <div className = "card">
+        <div className = "card-body card-image">
+          <img src="exemploFoto1.jpg"/>
+        </div>
+        <div className = "card-footer">
+          <Link className ="btn" to ="/MenuDiario" >Ações Diárias</Link>
+        </div>
+      </div>
+
+      <div className = "card">
+        <div className = "card-body card-image">
+          <img src="exemploFoto2.jpg"/>
+        </div>
+        <div className = "card-footer">
+          <Link className ="btn" to ="/MenuEmocoes" >Emoções</Link>
+        </div>
+      </div>
+    </div>
 
     </>
   )
