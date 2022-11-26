fc294ed8f49d204eab1caf9a7817f5d0c9b0b22a
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2022-11-26 20:59:24 +0000

    css é dificil...

--- src/Pages/MenuDiario.js
+++ src/Pages/MenuDiario.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link} from "react-router-dom"
+import  "./menuDiarioStyle.css"
 
 function MenuDiario() {
   return (
-    <div>MenuDiario</div>
+    <>
+      <header className = "homeMenuHeader">
+        <div className ="btn-menuDiario">
+          <Link className ="btn-menuD" to ="/HomeMenu" >Menu</Link>
+        </div>
+        <div className ="menuDiarioLogo">
+          <img className='logo-menuDiario' src='logo192.png' alt='logo'/>
+        </div>
+    </header>
+    <p className = "menuDiarioText">
+        Ações Diárias <br/>
+        Selecione a ação que deseja fazer neste momento.<br/>
+        Tempo de seleção: 3 segundos.<br/>
+    </p>
+    
+    </>
   )
 }
 
