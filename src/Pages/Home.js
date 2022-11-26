a9b09853b92f73c69ddec8acfe5ea3b039816095
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2022-11-26 10:25:22 +0000

    Home page (falta css)

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,30 @@
 import React from 'react'
 
+
+import { Link} from "react-router-dom"
+
 function Home() {
+
+
   return (
-    <div>Home</div>
+    <>
+    <header>
+        <div className ="homeLogo">
+          <img className='logo' src='logo192.png' alt='logo'/>
+          <p>let's talk, in pictures!</p>
+        </div>
+    </header>
+    <p className = "homeText">
+        Aqui ajudamos-lhe a comunicar com os seus familiares e amigos!<br/>
+        Expresse o que está a sentir e o que deseja fazer no momento!<br/>
+        <br/>
+        <br/>
+        Este site está criado de forma a usar os seus olhos.<br/>
+        Para selecionar olhe para o botão desejado durante 3 segundos.<br/>
+    </p>
+    <Link className ="homeButton" to ="/HomeMenu" >Começar agora</Link>
+
+    </>
   )
 }
 
