830a158a851793c3ac9d4e3f03bc28bc871dcb8e
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2022-12-01 11:39:09 +0000

    carousel em construção

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,40 +4,63 @@ import records from "./recordsAçõesDiárias.json"
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
+import {FaArrowRight, FaArrowLeft } from "react-icons/fa"
 
 
 function Carousel(Index){
 
+    const NextArrow =({onClick}) => {
+        return (
+            <div className="arrow next" onClick={onClick}>
+                <FaArrowRight />
+            </div>
+        )
+    }
+
+    const PrevArrow =({onClick}) => {
+        return (
+            <div className="arrow prev" onClick={onClick}>
+                <FaArrowLeft />
+            </div>
+        )
+    }
+
     const record = records;
     let itemEscolhidoArray = Object.values(Index);
     let itemEscolhido = itemEscolhidoArray[0];
-    console.log(itemEscolhido);
+   
+    const[imageIndex, setImageIndex] = useState(itemEscolhido)
 
     const settings = {
         
         infinite: true,
-        speed: 500,
+        lazyLoad: true,
+        speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
-        initialSlide: itemEscolhido -1
+        initialSlide: itemEscolhido,
+
+        centerMode: true,
+        centerPadding: 0,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        beforeChange: (current, next) => setImageIndex(next)
       };
 
  
     return (
         <>
-        <h1>Funciona</h1>
         <Slider {...settings}>
         {record.map((item)=>(
-           <div key={item.id} className = "card">
-            <div className = "card-top">
-                <h3>{item.title}</h3>
-                <img src={item.image} alt ="fazer" />
-                <h1>{item.title}</h1>
+            <>
+            <div className={item.id === imageIndex ? "text-Description active-text-Description" :"text-Description"}>
+                <p>Está<br/><b>{item.title}</b></p>
             </div>
-            <div className="card-bottom">
-                <button>Ativar som </button>
-            </div>
+           <div key={item.id} className = {item.id === imageIndex ? "card-Carousel active-card-Carousel" :"card-Carousel"}>
+                <img className = "card-top" src={item.image} alt ="fazer" />
            </div> 
+           <button className= {item.id === imageIndex ? "card-bottom active-card-bottom" :"card-bottom"}>Ativar som </button>
+           </>
         ))}
         </Slider>
         </>
