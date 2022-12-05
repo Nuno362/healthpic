4420a9d62116408e0e3c7e90be7a22a0500feca8
Nuno362 <82151182+Nuno362@users.noreply.github.com>
2022-12-05 16:07:32 +0000

    mudei a 2 pagina

--- src/Pages/HomeMenu.js
+++ src/Pages/HomeMenu.js
@@ -19,18 +19,12 @@ function HomeMenu() {
  
     <div className = "card-grid ">
       <div className = "card firstElement">
-        <div className = "card-body card-image">
-          <img src="exemploFoto1.jpg"/>
-        </div>
         <div className = "card-footer">
           <Link className ="btn-homeMenu" to ="/MenuDiario" >Ações Diárias</Link>
         </div>
       </div>
 
       <div className = "card lastElement">
-        <div className = "card-body card-image">
-          <img src="exemploFoto2.jpg"/>
-        </div>
         <div className = "card-footer">
           <Link className ="btn-homeMenu" to ="/MenuEmocoes" >Emoções</Link>
         </div>
